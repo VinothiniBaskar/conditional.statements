4d9cd1ca1cd335eec0950cb5b4a97ce9a72727f6
VinothiniBaskar <113995562+VinothiniBaskar@users.noreply.github.com>
2023-01-11 15:33:40 +0530

    Add files via upload

---
+++ programs/ReversedNumber.java
@@ -0,0 +1,21 @@
+package string.programs;
+
+public class ReversedNumber {
+
+	public static void main(String[] args) {
+		int number=151,reminder;
+		int reverse=0;
+		System.out.println("The  number is :"+number);
+		while(number!=0) {
+			reminder=number%10;
+			reverse=reverse*10+reminder;
+			number=number/10;
+		}
+		
+		System.out.println("The reversed number is :"+reverse);
+		
+
+	}
+
+}
+ 
